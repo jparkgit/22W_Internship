92bcd0c9cd59f01b71ee00047fd009af2b926a5e
jparkgit <80691989+jparkgit@users.noreply.github.com>
2022-01-24 18:04:02 +0900

    Update GSE149337.r

--- GSE149337.r
+++ GSE149337.r
@@ -1,4 +1,11 @@
+
+
 gse149377 <- read.delim(file="GSE149377_iPSCderived_myeloid_cells_counts.gct.gz", skip=2)
+
+
+
+
+
 
 ###covert gene ids with biomart
 
@@ -13,6 +20,107 @@ gse149377$ensembl_gene_id = ""
 
 #input ensemble gene id
 gse149377["ensembl_gene_id"] = lapply("ensembl_gene_id", function(x) G_list[[x]][match(gse149377$Name, G_list$entrezgene_id)])
+
+
+
+
+
+## Pre-filtering [PCG filtering]
+# 전체 유전자의 info가 담긴 gmt파일을 불러와 그중 pcg만 필터링
+
+library(dplyr)
+
+ann = rtracklayer::import("/spstorage/DB/NGS_reference/GRCh38_v34/gencode.v34.primary_assembly.annotation.gtf") %>% as.data.frame()
+pcg = ann %>% filter(gene_type == "protein_coding") %>% distinct(gene_id)
+pcg$gene_id <- gsub("\\.[0-9]+","",pcg$gene_id)
+
+# GSE데이터에서 pcg 필터링
+
+pcglist <- ifelse(gse149377$ensembl_gene_id %in% pcg$gene_id, "True", "False")
+gse149377_pcg <- gse149377[which(pcglist == 'True'),]
+
+
+## 여기서부터 PBMC, IPS 나눠서 작업
+ips_pcg <- gse149377_pcg %>% select(ensembl_gene_id, contains("IPS"))
+pbmc_pcg <- gse149377_pcg %>% select(ensembl_gene_id, contains("PBMC"))
+
+# m0
+temp <- pbmc_pcg %>% select(ensembl_gene_id, contains("m0"))
+temp$Mean <- apply(temp[,-1], 1, mean)
+pbmc_m0 <- temp %>% filter(Mean >= 5) %>% select(-Mean)
+temp <- NULL
+dim(pbmc_m0) # 12657 6
+
+# m1
+temp <- pbmc_pcg %>% select(ensembl_gene_id, contains("m1"))
+temp$Mean <- apply(temp[,-1], 1, mean)
+pbmc_m1 <- temp %>% filter(Mean >= 5) %>% select(-Mean)
+temp <- NULL
+dim(pbmc_m1) #12906 6
+
+# m2
+temp <- pbmc_pcg %>% select(ensembl_gene_id, contains("m2"))
+temp$Mean <- apply(temp[,-1], 1, mean)
+pbmc_m2 <- temp %>% filter(Mean >= 5) %>% select(-Mean)
+temp <- NULL
+dim(pbmc_m2) #12958     6
+
+# monocyte
+temp <- pbmc_pcg %>% select(ensembl_gene_id, contains("mono"))
+temp$Mean <- apply(temp[,-1], 1, mean)
+pbmc_mono <- temp %>% filter(Mean >= 5) %>% select(-Mean)
+temp <- NULL
+dim(pbmc_mono) # 11996     9
+
+# PBMC는 microglia 없음
+
+
+## 두 데이터씩 합치기
+pbmc_mono_m0 <- pbmc_mono %>% inner_join(pbmc_m0, by = "ensembl_gene_id")
+pbmc_m0_m1 <- pbmc_m0 %>% inner_join(pbmc_m1, by = "ensembl_gene_id")
+pbmc_m0_m2 <- pbmc_m0 %>% inner_join(pbmc_m2, by = "ensembl_gene_id")
+
+
+
+
+# create metadata
+#mono m0
+names <- colnames(pbmc_mono_m0)
+names <- names[-1]
+meta_pbmc_mono_m0 <- data.frame(names)
+meta_pbmc_mono_m0$type = ""
+meta_pbmc_mono_m0$type <- ifelse(grepl("mono", meta_pbmc_mono_m0$names), "mono", "m0")
+
+#m0 m1
+names <- colnames(pbmc_m0_m1)
+names <- names[-1]
+meta_pbmc_m0_m1 <- data.frame(names)
+meta_pbmc_m0_m1$type = ""
+meta_pbmc_m0_m1$type <- ifelse(grepl("m0", meta_pbmc_m0_m1$names), "m0", "m1")
+
+#m0 m2
+names <- colnames(pbmc_m0_m2)
+names <- names[-1]
+meta_pbmc_m0_m2 <- data.frame(names)
+meta_pbmc_m0_m2$type = ""
+meta_pbmc_m0_m2$type <- ifelse(grepl("m0", meta_pbmc_m0_m2$names), "m0", "m2")
+
+
+
+
+cp /spstorage/INTERNSHIP/jpark/prefiltering_done.RData /spstorage/INTERNSHIP/hyeseon
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ### [Unsolved] Codes for autonaming types of cells 
