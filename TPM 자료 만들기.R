7ae62c677ccf681acc2e98a956e29da2abc2ab3e
sehan25 <98152279+sehan25@users.noreply.github.com>
2022-01-25 11:24:21 +0900

    Update TPM 자료 만들기.R

--- TPM 자료 만들기.R
+++ TPM 자료 만들기.R
@@ -28,3 +28,23 @@ GSE11382_tpm <- data.frame(GSE11382_genes$Feature.ID, GSE11382_tpm_counts)
 colnames(GSE11382_tpm)[1] <- 'Feature.ID'
 head(GSE11382_tpm)
 ############
+
+
+############
+# t-SNE
+index <- rep(c("hematopoietic_stem_cells","common_myeloid_progenitors",
+	"granulocyte-macrophage_progenitors","megakaryocyte-erythrocyte_progenitors"), each=5)
+metadata <- data.frame(index)
+rownames(metadata) <- colnames(GSE11382_tpm)[-1]
+
+# dds <- DESeqDataSetFromMatrix(countData = GSE11382_tpm,
+#                                colData = metadata,
+#                                design = ~index, tidy = TRUE)
+# dds_tpm <- DESeq(dds)
+# res_tpm <- results(dds_tpm)
+# head(res_tpm)
+
+library(Rtsne)
+GSE11382_tpm_sample <- GSE11382_tpm %>% sample_n(1000)
+tsne_tpm <- Rtsne(GSE11382_tpm_sample, perplexity=30, check_duplicates=FALSE)
+plot(tsne_tpm$Y, col = "black", bg = metadata$index, pch = 21, cex = 1)
